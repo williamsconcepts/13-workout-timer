8e72ceca2ed68c2f36d65badfeefe88f1b21505e
williamsconcepts <chidiekwebelam@gmail.com>
2026-02-09 05:22:26 +0100

    Closures in Effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
+
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -7,15 +8,49 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
+  const [duration, setDuration] = useState(0);
 
-  const playSound = function () {
+  //   const playSound = useCallback (function () {
+  //   if (!allowSound) return;
+  //   const sound = new Audio(clickSound);
+  //   sound.play();
+  // }, [allowSound]);   
+
+  useEffect(function () {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak]);
+
+  useEffect(function () {
+    const playSound = function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
-  };
+  };  
+playSound();
+  }, [duration, allowSound]);
+
+
+  useEffect(function () {
+    console.log(duration, sets)
+    document.title = `Your ${number}-exercise workout`;
+  }, [number, duration, sets]);
+
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+
+
+  function handleInc() {
+    setDuration((duration) => Math.floor(duration) + 1);
+   
+  }
+
+  function handleDec() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+   
+  }
 
   return (
     <>
@@ -61,18 +96,19 @@ function Calculator({ workouts, allowSound }) {
             max='10'
             value={durationBreak}
             onChange={(e) => setDurationBreak(e.target.value)}
+            
           />
           <span>{durationBreak} minutes/break</span>
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
